e5cb59de50929cfb500932e002e7171c179f392e
suxiaobo0827 <21215655+suxiaobo0827@users.noreply.github.com>
2026-08-27 14:24:27 +0800

    feat: add mobile tabbar

---
+++ src/components/AppTabbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <van-tabbar v-model="active" route safe-area-inset-bottom>
+    <van-tabbar-item replace to="/" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item replace to="/profile" icon="user-o">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const active = computed(() => (route.path === '/profile' ? 1 : 0))
+</script>
