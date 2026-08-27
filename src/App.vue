5e7066479e8ae1050e0c2fe1ce595178ea7d4e08
suxiaobo0827 <21215655+suxiaobo0827@users.noreply.github.com>
2026-08-27 14:24:09 +0800

    feat: add app shell

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-shell">
+    <main class="app-content">
+      <RouterView />
+    </main>
+
+    <AppTabbar />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import AppTabbar from './components/AppTabbar.vue'
+</script>
