3f96dc6f7a0f950d91422dd963dfdf6454d1806f
suxiaobo0827 <21215655+suxiaobo0827@users.noreply.github.com>
2026-08-27 14:35:33 +0800

    feat: configure px to vw conversion

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pxToViewport from 'postcss-px-to-viewport-8-plugin'
 
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        pxToViewport({
+          unitToConvert: 'px',
+          viewportWidth: 375,
+          unitPrecision: 6,
+          propList: ['*'],
+          viewportUnit: 'vw',
+          fontViewportUnit: 'vw',
+          selectorBlackList: ['.ignore-vw'],
+          minPixelValue: 1,
+          mediaQuery: false,
+          replace: true,
+          exclude: [/node_modules/],
+        }),
+      ],
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
